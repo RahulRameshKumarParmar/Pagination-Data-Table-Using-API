f9a1b5fa87e7f20355dbdcd8efeacc41836c5bd6
Rahul Parmar <rahulparmar1339950@gmail.com>
2026-01-13 22:16:19 +0530

    Created Data Table

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,55 @@
-interface Data{
+interface Data {
+  id: number;
   title: string;
-  placeOfOrigin: string;
-  artist: string;
-  inscription: null | string;
-  startingDate: string;
-  endDate: string;
+  place_of_origin: string;
+  artist_display: string;
+  inscriptions: null | string;
+  date_start: number;
+  date_end: number;
 }
 
 import "./App.css";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-
   const [allData, setAllData] = useState<Data[]>([]);
   const [page, setPage] = useState(1);
+  const [selectedRow, setSelectedRow] = useState<Data[] | null>([]);
 
-  const getData = async () => {
-    try {
-      const response = await fetch(`https://api.artic.edu/api/v1/artworks?page=${page}`);
-      const data = await response.json();
-      setAllData(data.data);
-    }
-    catch (error) {
-      console.error("Error is:", error);
-    }
-  }
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await fetch(
+          `https://api.artic.edu/api/v1/artworks?page=${page}`
+        );
+        const data = await response.json();
+        setAllData(data.data);
+      } catch (error) {
+        console.error("Error is:", error);
+      }
+    };
 
-  getData();
+    getData();
+  }, [page]);
+
   return (
     <>
-      <DataTable>
+      <DataTable
+        value={allData}
+        dataKey="id"
+        selection={selectedRow}
+        onSelectionChange={(e) => setSelectedRow(e.value)}
+      >
         <Column selectionMode="multiple" headerStyle={{ width: "3rem" }} />
 
         <Column field="title" header="TITLE" />
-        <Column field="place of origin" header="PLACE OF ORIGIN" />
-        <Column field="artist" header="ARTIST" />
+        <Column field="place_of_origin" header="PLACE OF ORIGIN" />
+        <Column field="artist_display" header="ARTIST" />
         <Column field="inscriptions" header="INSCRIPTIONS" />
-        <Column field="start date" header="START DATE" />
-        <Column field="end date" header="END DATE" />
+        <Column field="date_start" header="START DATE" />
+        <Column field="date_end" header="END DATE" />
       </DataTable>
     </>
   );
